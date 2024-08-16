feae939ac39b7d50af67c89af5486dcc096f02d5
Praveen kumar <bandipraveenkumar78@gmail.com>
2024-08-16 09:54:24 +0530

    connection to database was done afer dividing tables

--- Main.py
+++ Main.py
@@ -40,6 +40,71 @@ transactions_df = pd.DataFrame(transaction_data)
 # Merging of the both tables
 
 merged_df = pd.merge(transactions_df, customers_df, on='customer_id', how='inner')
-print("Merged DataFrame Info:")
-print(merged_df.info())
+# print("Merged DataFrame Info:")
+# print(merged_df.info())
 
+
+
+#Seperating the tables and connecting to the data base 
+
+from sqlalchemy import create_engine
+
+# Load JSON data
+with open('D:/REV Projects/Project_0/DataSets/customers.json', 'r') as file:
+    customers_data = json.load(file)
+with open('D:/REV Projects/Project_0/DataSets/transaction_logs.json', 'r') as file:
+    transactions_data = json.load(file)
+
+# Convert to DataFrames
+customers_df = pd.DataFrame(customers_data)
+transactions_df = pd.DataFrame(transactions_data)
+
+# Transform Customers DataFrame
+customers_df = customers_df[['customer_id', 'customer_name', 'country', 'city']]
+
+# Transform Products DataFrame
+products_df = pd.DataFrame({
+    'product_id': transactions_df['product_id'].unique(),
+    'product_name': transactions_df.drop_duplicates('product_id')['product_name'],
+    'product_category': transactions_df.drop_duplicates('product_id')['product_category']
+})
+
+# Transform Ecommerce Websites DataFrame
+ecommerce_websites_df = pd.DataFrame({
+    'website_id': range(len(transactions_df['ecommerce_website_name'].unique())),
+    'website_name': transactions_df['ecommerce_website_name'].unique()
+})
+
+# Create a mapping from website_name to website_id
+website_name_to_id = ecommerce_websites_df.set_index('website_name')['website_id'].to_dict()
+transactions_df['website_id'] = transactions_df['ecommerce_website_name'].map(website_name_to_id)
+
+# Transform Transactions DataFrame
+transactions_df['datetime'] = pd.to_datetime(transactions_df['datetime'])
+transactions_df = transactions_df[['order_id', 'customer_id', 'product_id', 'website_id', 'qty', 'price', 'datetime', 'payment_type', 'payment_txn_id', 'payment_txn_success', 'failure_reason']]
+
+# Create SQLAlchemy engine
+engine = create_engine('mysql+pymysql://root:1111@localhost/revp0')
+
+# Insert data into tables
+customers_df.to_sql('customers', con=engine, if_exists='replace', index=False)
+products_df.to_sql('products', con=engine, if_exists='replace', index=False)
+ecommerce_websites_df.to_sql('ecommerce_websites', con=engine, if_exists='replace', index=False)
+transactions_df.to_sql('transactions', con=engine, if_exists='replace', index=False)
+
+
+
+import mysql.connector
+
+db=mysql.connector.connect(
+    host ="localhost",
+    user ="root",
+    password='1111',
+    database="revp0"
+) 
+cursor=db.cursor()     #cursor used for running the sql queries.
+cursor.execute("select *from customers limit 5")
+rows=cursor.fetchmany(size=4)
+res=cursor.fetchall()
+for _ in res:
+    print(_)
